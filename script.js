f6188f0384fe0b1c51d5bea3da9f8fe9dc1ff376
6Alexandre7 <alexandre.eh67@gmail.com>
2022-01-23 14:53:32 +0100

    Améliorations  HTML + CSS

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 
 // Elément parent qui contient toutes les cartes
-const container = document.querySelector(".most_popular");
 const billboard = document.getElementById("billboard");;
 
+const most_popular = document.getElementById("most-popular");
+const harry_potter = document.getElementById("harry-potter");
 
 var dict = {};
 
 
 
-
-function Scroll(direction){
+function Scroll(scroll_view, direction){
    if (direction === 'Left'){
-      container.scroll(container.scrollLeft - 160*7, 0);
+      scroll_view.scroll(scroll_view.scrollLeft - 160*7, 0);
    }
    else{
-      container.scroll(container.scrollLeft + 160*7, 0);
+      scroll_view.scroll(scroll_view.scrollLeft + 160*7, 0);
    }
 }
 
-function getMovies(url) {
+function getMovies(movie_container, url) {
    fetch(url).then(res => res.json()).then(data => {
       Create_BillBoard(data.results[Math.ceil( Math.random()*10 )]);
       data.results.slice(1).forEach(movie => {
-         Create_Card_Movie(movie);
+         Create_Card_Movie(movie_container, movie);
       });
    })
 }
@@ -41,49 +41,27 @@ function Create_BillBoard(movie){
 }
 
 
-function Create_Card_Movie(movie){
+function Create_Card_Movie(movie_container, movie){
    // Créer la carte
    let card = document.createElement('div');
-   container.appendChild(card);
+   movie_container.appendChild(card);
 
    if (movie.backdrop_path !== null){
    // THUMBNAIL
-   /*
-      // Mettre la backdrop_image du film
-      let test = document.createElement('img');
-      test.src = `https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`
-      card.appendChild(test);
-
-      let original = document.createElement('p');
-      original.innerHTML = `${movie.original_title}`
-      card.appendChild(original);
-
-      let titre = document.createElement('p');
-      titre.innerHTML = `${movie.title}`
-      card.appendChild(titre);
-      */
-   // Coup d'oeil
-      // Mettre la backdrop_image du film
-      /*
-      let backdrop_image = document.createElement('img');
-      backdrop_image.src = `https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`
-      card.appendChild(backdrop_image);
-      */
       url_image = `https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`
-      
+      card.classList.add("card")
       card.innerHTML = 
       `
-      <input type="image" src="${url_image}" alt="${movie.title}" onclick="f(${movie.id})">
+         <input type="image" src="${url_image}" alt="${movie.title}" onclick="f(${movie.id})">
+         <!-- 
+         <div class="previsualisation-card">
+            <img src="${url_image}" alt="${movie.title}">
+         </div> 
+         -->
       `;
 
       dict[movie.id] = movie;
       
-   }
-   else{
-      // Mettre le poster du film
-      let cover = document.createElement('img');
-      cover.src = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
-      card.appendChild(cover);
    }
 }
 
@@ -93,10 +71,9 @@ function f(movie){
 }
 
 
-getMovies(`${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}&language=fr-FR`);
+getMovies(most_popular, `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}&language=fr-FR`);
 
-
-//getMovies(`https://api.themoviedb.org/3/search/movie?api_key=9589339f8da90a06aed6e3d2b11f4901&language=fr-FR&query=Harry Potter`);
+getMovies(harry_potter, `https://api.themoviedb.org/3/search/movie?api_key=9589339f8da90a06aed6e3d2b11f4901&language=fr-FR&query=Harry Potter`);
 //getMovies(`http://api.tmdb.org/3/search/person?api_key=9589339f8da90a06aed6e3d2b11f4901&query=Jonathan`);
 
 
