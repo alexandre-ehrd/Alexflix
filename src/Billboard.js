c7f0a5d2440055fe2168e3e38fd1cb0595911e36
6Alexandre7 <alexandre.eh67@gmail.com>
2022-02-05 23:14:21 +0100

    billboard improvement

--- src/Billboard.js
+++ src/Billboard.js
@@ -1,15 +1,34 @@
-function Billboard(movie){
+function Billboard(movie, popularity_rank){
 	return (`
-      <img src="https://images.unsplash.com/photo-1644063414496-5e48002d614b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" width="100%">
-      <div id="billboard-info">
-	      <h1>{movie.title}</h1>
-	      <p>{movie.overview}</p>
-         <div>
-         <p>Ma liste</p>
-         <p>Plus d'infos</p>
+      <section class="billboard">
+         <img src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="" width="100%"></img>
+         <div class="billboard-informations">
+            <div class="billboard-movie-title">
+               <h1>${movie.title}</h1>
+            </div>
+   
+            <div class="billboard-movie-rank">
+               <i class="bi bi-award"></i>
+               <p>N°${popularity_rank} en France aujourd'hui </p>
+            </div>
+   
+            <div class="billboard-movie-synopsis">
+               <p>${movie.overview}</p>
+            </div>
+   
+            <div class="billboard-movie-buttons-container">
+               <div class="billboard-movie-button" id="billboard-movie-button-liste">
+                  <i class="bi bi-bookmark-plus"></i>
+                  <p>Ma liste</p>
+               </div>
+               <div class="billboard-movie-button" id="billboard-movie-button-information">
+                  <i class="bi bi-info-circle"></i>
+                  <p>Plus d'infos</p>
+               </div>
+            </div>
          </div>
-      </div>
-      <div id="billboard-shadow"></div>
+         <div class="billboard-shadow-bottom"></div>
+      </section>
 	`);
 	
 }
--- src/script.js
+++ src/script.js
@@ -121,7 +121,7 @@ function GetMovies_URL(movie_container, URL){
 	fetch(URL).then(response => {
 	if (response.ok){
 		response.json().then(list_movies => {
-			Create_BillBoard(list_movies['results'][0])
+			console.log(list_movies)
 		   for (let i_movie = 0; i_movie < list_movies['results'].length; i_movie++) {
             // Créer les cartes de chaque films
 		      Create_Card_Movie(movie_container, list_movies['results'][i_movie]);
@@ -160,7 +160,7 @@ function Create_Card_Movie(movie_container, movie){
 }
 
 /* GetMovies_URL(most_popular, `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&language=fr-FR`);
-GetMovies_URL(harry_potter, `https://api.themoviedb.org/3/search/movie?api_key=9589339f8da90a06aed6e3d2b11f4901&language=fr-FR&query=Harry Potter`);
+GetMovies_URL(harry_potter, `https://api.themoviedb.org/3/search/movie?api_key=9589339f8da90a06aed6e3d2b11f4901&language=fr-FR&query=Harry Potter 20th`);
 Open_Movies_Genres(); */
 
 
@@ -170,7 +170,33 @@ Open_Movies_Genres(); */
 import {Header} from './Header.js';
 import {Billboard} from './Billboard.js';
 
-BODY.innerHTML = Header();
-BODY.innerHTML += Billboard();
+/* BODY.innerHTML = Header();
+BODY.innerHTML += Billboard(); */
+
+GetMovies_URL(harry_potter, "https://api.themoviedb.org/3/search/movie?api_key=9589339f8da90a06aed6e3d2b11f4901&language=fr-FR&query=Harry%20Potter");
 
 
+
+const obj = {
+	"adult": false,
+	"backdrop_path": "/sixfWYfNegaGGHKdXrNNUHaMiAC.jpg",
+	"genre_ids": [
+	36,
+	18,
+	53,
+	10752
+	],
+	"id": 205596,
+	"original_language": "en",
+	"original_title": "The Imitation Game",
+	"overview": "1940 : Alan Turing, mathématicien, cryptologue, est chargé par le gouvernement Britannique de percer le secret de la célèbre machine de cryptage allemande Enigma, réputée inviolable. À la tête d'une équipe improbable de savants, linguistes, champions d'échecs et agents du renseignement, Turing s'attaque au chef-d'œuvre de complexité dont la clef peut conduire à la victoire. IMITATION GAME relate la façon dont Alan Turing, soumis à une intense pression, contribua à changer le cours de la Seconde Guerre mondiale et de l'Histoire. C'est aussi le portrait d'un homme qui se retrouva condamné par la société de l'époque en raison de son homosexualité et en mourut.",
+	"popularity": 60.176,
+	"poster_path": "/gBxCZuieAe8KClWWP1vVijXBlTp.jpg",
+	"release_date": "2014-11-14",
+	"title": "Imitation Game",
+	"video": false,
+	"vote_average": 8,
+	"vote_count": 14280
+}
+
+BODY.innerHTML += Billboard(obj, 4);
